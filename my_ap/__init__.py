bcedadc047fd6a0db41e88ddf3df8032732babf6
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-04-12 10:30:14 -0300

    :lock: fix: Adicionando polcitica de segurança CORS

--- my_ap/__init__.py
+++ my_ap/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
