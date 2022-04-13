39bbee82c9971d7ec8f826f8516d5f309caad966
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-04-13 09:33:07 -0300

    :sparkles: feat: Adicionando botoes para update e delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import ArticleList from './components/ArticleList';
 
 function App() {
   const [articles, setArticles] = useState([]);
-
+  const [editedArticle, setEditedArticle] = useState([]);
   useEffect(() => {
      fetch('http://127.0.0.1:8090/api/get', {
         'method': 'GET',
@@ -17,11 +17,14 @@ function App() {
      .then(resp => setArticles(resp))
      .catch(error => console.log(error))
   }, [])
-
+  const editArticle = (article) => {
+      console.log("Hello worlds")
+      //setEditedArticle(article)
+  }
   return (
     <div className="App">
       <h1>Flask and ReactJS Course</h1>
-        <ArticleList  articles = {articles}/>
+        <ArticleList  articles = {articles} editArticle = {editArticle}/>
     </div>
   );
 }
--- frontend/src/components/ArticleList.js
+++ frontend/src/components/ArticleList.js
@@ -2,6 +2,11 @@
 import React from 'react';
 
 function ArticleList(props){
+
+    const editArticle = (article) => {
+        props.editArticle(article)
+    }
+
     return (
         <div>
             {props.articles && props.articles.map( article =>{
@@ -10,6 +15,20 @@ function ArticleList(props){
                 <h2>{article.title}</h2>
                 <p>{article.body}</p>
                 <p>{article.date}</p>
+
+                <div className='row'>
+                    <div className='col-md-1'>
+                        <button className='btn btn-primary'
+                        onClick={() => editArticle(article)}>Update</button>
+
+                    </div>
+
+                    <div className='col-md-1'>
+                        <button className='btn btn-danger'>Delete</button>
+                    </div>
+                </div>
+                
+                <hr/>
 
             </div>
         )})}
